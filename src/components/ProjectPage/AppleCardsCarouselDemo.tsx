c397457d90a65f1f306792d93d283a8a58e2339d
karthik <karthikajay2020@gmail.com>
2025-12-06 17:52:59 +0530

    Updated portfoilio link

--- src/components/ProjectPage/AppleCardsCarouselDemo.tsx
+++ src/components/ProjectPage/AppleCardsCarouselDemo.tsx
@@ -144,7 +144,7 @@ const PortfolioContent = () => {
             <div className="w-full rounded-xl">
                 <PortfolioScroll />
             </div>
-            <GithubButton href="https://github.com/karthikajay04/PortfolioWebsite" />
+            <GithubButton href="https://github.com/karthikajay04/karthikajay" />
         </div>
     );
 };
